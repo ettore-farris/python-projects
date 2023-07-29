c42f63be779ee790289217d28316fb92d5a0ee85
ettorefarris <72468754+ettorefarris@users.noreply.github.com>
2023-07-30 01:33:58 +0200

    Add files via upload

---
+++ Convertitore alfabeto telefonico/READ_ME.txt
@@ -0,0 +1,3 @@
+Convertitore di parole in alfabeto telefonico, utile per lo spelling.
+Es: "Matteo" = "Milano", "Ancona", "Torino", "Torino", "Empoli", "Otranto"
+Semplice applicazione sviluppata con Python.
---
+++ Convertitore alfabeto telefonico/convertitore_alfabeto_telefonico.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+name = input("Scrivi il tuo nome: ").lower()
+
+alfabeto_telefonico = {
+    "lettere": [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r","s","t","u","v","w","x","y","z"],
+    "nomi": ["Spazio", "Ancona","Bari","Como","Domodossola","Empoli", "Firenze", "Genova", "Hotel", "Imola", "I lunga", "Cappa","Livorno", "Milano", "Napoli", "Otranto", "Palermo", "Quadro", "Roma", "Savona", "Torino", "Udine", "Venezia", "Vu doppia", "Ics", "Ipsilon", "Zeta"]  
+}
+
+df = pd.DataFrame(alfabeto_telefonico)
+
+result = [row.nomi for l in name for (index, row) in df.iterrows() if l == row.lettere]
+print(result)
+
+
+
+
