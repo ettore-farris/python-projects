0e312886f98d6b77af129850a56dfe935cf4e861
ettorefarris <72468754+ettorefarris@users.noreply.github.com>
2023-07-30 18:38:22 +0200

    Update convertitore_alfabeto_telefonico.py

--- Convertitore alfabeto telefonico/convertitore_alfabeto_telefonico.py
+++ Convertitore alfabeto telefonico/convertitore_alfabeto_telefonico.py
@@ -1,17 +1,22 @@
 import pandas as pd
 
-name = input("Scrivi il tuo nome: ").lower()
-
 alfabeto_telefonico = {
-    "lettere": [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r","s","t","u","v","w","x","y","z"],
-    "nomi": ["Spazio", "Ancona","Bari","Como","Domodossola","Empoli", "Firenze", "Genova", "Hotel", "Imola", "I lunga", "Cappa","Livorno", "Milano", "Napoli", "Otranto", "Palermo", "Quadro", "Roma", "Savona", "Torino", "Udine", "Venezia", "Vu doppia", "Ics", "Ipsilon", "Zeta"]  
+    "lettere": [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+    "nomi": ["Spazio", "Ancona", "Bari", "Como", "Domodossola", "Empoli", "Firenze", "Genova", "Hotel", "Imola", "I lunga", "Cappa", "Livorno", "Milano", "Napoli", "Otranto", "Palermo", "Quadro", "Roma", "Savona", "Torino", "Udine", "Venezia", "Vu doppia", "Ics", "Ipsilon", "Zeta"]
 }
 
 df = pd.DataFrame(alfabeto_telefonico)
+result = []
 
-result = [row.nomi for l in name for (index, row) in df.iterrows() if l == row.lettere]
-print(result)
-
+while True:
+    name = input("Scrivi il tuo nome: ").lower()
+    nospacename = name.replace(' ', '')
+    if nospacename.isalpha() == False:
+        print("Digita solo lettere!")
+    else:
+        result = [row.nomi for l in name for (index,row) in df.iterrows if l == row.letters]
+        print(result)
+        break
 
 
 
